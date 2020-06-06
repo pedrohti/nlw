cf44be4b766b4c766295fed7d7ff345d69b4453a
Pedro <pedroh.ti@hotmail.com>
2020-06-06 13:41:13 -0300

    Adicionado suporte ao snippet do Nunjucks

--- src/server.js
+++ src/server.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const app = express();
-
 const nunjucks = require("nunjucks");
+
 nunjucks.configure("src/views", {
-	express: server,
+	express: app,
 	noCache: true,
 });
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => res.render("index.html"));
+app.get("/", (req, res) => {
+	return res.render("index.html");
+});
 app.get("/novoponto", (req, res) =>
 	res.sendFile(__dirname + "/views/create-point.html"),
 );
-app.get("/buscarponto", (req, res) =>
-	res.sendFile(__dirname + "/views/search-results.html"),
-);
+app.get("/search", (req, res) => {
+	return res.render("search-results.html");
+});
 
 app.listen(3000, () => console.log("http://localhost:3000"));
